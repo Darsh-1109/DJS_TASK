8359a4f744630ff1934dc54801c5493ea5679079
Darsh-1109 <120316022+Darsh-1109@users.noreply.github.com>
2022-12-11 18:38:07 +0530

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,31 @@
+import string
+
+
+drivers_name=["lEwis_HaMiLton","MaX_vERsTapPen","SebASTIan_vEtTeL","ChaRleS_IEcLeRc"]
+print(list(enumerate(drivers_name)))
+indices=[]
+driver=[]
+
+for c,d in enumerate(drivers_name):
+    indices.append(c)
+    driver.append(d)
+for i in range(0,4):
+    print(indices[i]) 
+    print(driver[i])
+for i in range (0,4):
+    driver[i]=driver[i].lower()
+    
+    driver[i]=driver[i].replace("_"," ")
+    print(driver[i])
+length = lambda val : len(val)
+temp=list(map(length,driver))
+print(f"{driver}")
+print(f"{temp}")
+z=zip(driver,temp)
+print(list(z))
+indices=[sum(i) for i in zip(indices,temp)]
+print(indices)
+indices.sort(reverse=True)
+print(indices)
+F1_drivers=dict(zip(driver,indices))
+print(F1_drivers)
